925a89548fa4b3d42ab3885dd6c1262301051a64
John Garland <johnnyg@cse.unsw.edu.au>
2010-08-04 12:32:02 +1000

    Support both am/pm and 24 hour times

--- timetable.py
+++ timetable.py
@@ -16,9 +16,7 @@ activities = {}
 
 for section in config.sections():
     (activity, sep, activity_type) = section.partition(':')
-    if activity not in activities:
-        activities[activity] = {}
-    activities[activity][activity_type] = {}
+    activities.setdefault(activity, {})[activity_type] = {}
     for option in config.options(section):
         option_value = config.get(section, option)
         if option == "weeks":
@@ -34,6 +32,24 @@ for section in config.sections():
                         last = first
                     weeks.extend(range(int(first),int(last)+1))
                 option_value = weeks
+        elif option == "time":
+            (day, sep, time) = option_value.partition(' ')
+            (start, sep, end) = time.partition('-')
+            if end.endswith("pm"):
+                offset = 12
+                end = end.replace("pm","")
+            else:
+                offset = 0
+                end = end.replace("am","")
+            end = (int(end) + offset) % 24
+            if start.endswith("am"):
+                offset = 0
+                start = start.replace("am", "")
+            elif start.endswith("pm"):
+                offset = 12
+                start = start.replace("pm", "")
+            start = (int(start) + offset) % 24
+            option_value = "%s %d-%d" % (day, start, end)
         activities[activity][activity_type][option] = option_value
 
 pprint.pprint(activities, width=120)
